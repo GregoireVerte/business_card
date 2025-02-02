65359271ad829280c0056f6d138528cf1cea404b
Grzegorz Zielinski <trevor84@wp.pl>
2025-02-02 20:12:24 +0100

    add_base_and_business_contact_inheritance_classes

--- main.py
+++ main.py
@@ -5,11 +5,44 @@ from faker import Faker
 fake = Faker()
 
 
-class Business_card:
+class Business_card: ## Base Class
     def __init__(self, name, surname, email):
         self.name = name
         self.surname = surname
         self.email = email
+
+    @property
+    def label_length(self):
+        """Zwraca długość pełnego imienia i nazwiska (łącznie ze spacją)."""
+        return len(self.name + " " + self.surname)
+
+
+
+class BaseContact(Business_card):
+    def __init__(self, private_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.private_phone = private_phone
+
+    def contact(self):
+        """Wybiera prywatny numer telefonu."""
+        print(f"Wybieram numer {self.private_phone} i dzwonię do {self.name} {self.surname}.")
+
+
+
+class BusinessContact(Business_card):
+    def __init__(self, work_phone, company, position, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.work_phone = work_phone
+        self.company = company
+        self.position = position
+
+    def contact(self):
+        """Wybiera służbowy numer telefonu."""
+        print(f"Wybieram numer {self.work_phone} i dzwonię do {self.name} {self.surname}.")
+
+
+
+
 
 
 
