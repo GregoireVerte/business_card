82988863a961b4bb2399830f720cda163eb4c568
Grzegorz Zielinski <trevor84@wp.pl>
2025-02-02 18:46:10 +0100

    add_business_card_base_class

---
+++ main.py
@@ -0,0 +1,22 @@
+
+from faker import Faker
+
+
+fake = Faker()
+
+
+class Business_card:
+    def __init__(self, name, surname, email):
+        self.name = name
+        self.surname = surname
+        self.email = email
+
+
+
+
+
+
+
+
+
+
